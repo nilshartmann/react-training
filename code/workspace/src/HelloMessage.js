e711e267a21eac9057c3829120aa89b5956fc474
Nils Hartmann <nils@nilshartmann.net>
2018-09-19 19:14:28 +0200

    prepare new workshop

--- code/workspace/src/HelloMessage.js
+++ code/workspace/src/HelloMessage.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 export default class HelloMessage extends React.Component {
-  constructor(props) {
-    super(props);
-    const person = {
-      name: "",
-      age: 3
-    }
-  }
   render() {
-    return <React.Fragment>
-      <p>Yes</p>
-      <p>No</p>
-    </React.Fragment>
+    return <p>Hello, World</p>;
   }
 }
