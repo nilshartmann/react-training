4f8c55cf4b8a368887a71dfa1f123de5502df903
Nils Hartmann <nils@nilshartmann.net>
2019-05-19 09:05:23 +0200

    add hooks initial

---
+++ code/schritte/1-detail/src/GreetingDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function GreetingDetail(props) {
+  const [name, setName] = React.useState(props.initialName || "");
+  const [greeting, setGreeting] = React.useState(props.initialGreeting || "");
+  const inputRef = React.createRef();
+
+  function reset() {
+    setName("");
+    setGreeting("");
+
+    inputRef.current.focus();
+  }
+
+  return (
+    <div>
+      <input ref={inputRef} onChange={event => setName(event.target.value)} value={name} placeholder="Name" />
+      <input onChange={event => setGreeting(event.target.value)} value={greeting} name="greeting" placeholder="Greeting" />
+
+      <button onClick={reset}>Clear</button>
+    </div>
+  );
+}
---
+++ code/schritte/1b-detail-reducer/src/GreetingDetail.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+function greetingReducer(oldState, action) {
+  console.log("greeintReducer", oldState, action);
+  switch (action.type) {
+    case "UPDATE_GREETING":
+      return {
+        ...oldState,
+        greeting: action.greeting
+      };
+    case "UPDATE_NAME":
+      return {
+        ...oldState,
+        name: action.name
+      };
+    case "RESET":
+      return { greeting: "", name: "" };
+  }
+}
+
+export default function GreetingDetail(props) {
+  const [state, dispatch] = React.useReducer(greetingReducer, {
+    name: props.initialName,
+    greeting: props.initialGreeting
+  });
+  const inputRef = React.createRef();
+
+  console.log("state", state);
+
+  function reset() {
+    dispatch({
+      type: "RESET"
+    });
+    inputRef.current.focus();
+  }
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        onChange={event =>
+          dispatch({
+            type: "UPDATE_NAME",
+            name: event.target.value
+          })
+        }
+        value={state.name}
+        placeholder="Name"
+      />
+
+      <input
+        onChange={event =>
+          dispatch({
+            type: "UPDATE_GREETING",
+            name: event.target.value
+          })
+        }
+        value={state.greeting}
+        name="greeting"
+        placeholder="Greeting"
+      />
+
+      <button onClick={reset}>Clear</button>
+    </div>
+  );
+}
