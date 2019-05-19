4fbd1258dee2bf514795dfdbb88710b2e3a8b01b
Nils Hartmann <nils@nilshartmann.net>
2019-05-20 00:05:54 +0200

    add jest

---
+++ code/schritte/4-test/jest.config.js
@@ -0,0 +1,6 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  testResultsProcessor: "../../../node_modules/jest-html-reporter"
+};
