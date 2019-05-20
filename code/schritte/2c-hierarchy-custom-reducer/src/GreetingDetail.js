c35d235fdaa88a2ea4778ae9c4e9a0a00a6eb1c0
Nils Hartmann <nils@nilshartmann.net>
2019-05-20 11:18:24 +0200

    add custom hook example with reducer

---
+++ code/schritte/2c-hierarchy-custom-reducer/src/GreetingDetail.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function GreetingDetail(props) {
+  const [name, setName] = React.useState(props.initialName || "");
+  const [greeting, setGreeting] = React.useState(props.initialGreeting || "");
+
+  const input = React.useRef();
+
+  const saveDisabled = !(name && greeting);
+
+  function reset() {
+    setName("");
+    setGreeting("");
+
+    input.current.focus();
+  }
+
+  function save() {
+    props.onSave({
+      name,
+      greeting
+    });
+  }
+
+  return (
+    <div>
+      <input
+        ref={input}
+        onChange={event => setName(event.target.value)}
+        value={name}
+        placeholder="Name"
+      />
+      <input
+        onChange={event => setGreeting(event.target.value)}
+        value={greeting}
+        placeholder="Greeting"
+      />
+
+      <button onClick={reset}>Clear</button>
+      <button disabled={saveDisabled} onClick={save}>
+        Save
+      </button>
+    </div>
+  );
+}
