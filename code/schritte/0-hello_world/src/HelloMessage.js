6b70dd98500ed1c6fe8c232b07763b13962b9551
Nils Hartmann <nils@nilshartmann.net>
2019-06-28 09:32:50 +0200

    prepare

--- code/schritte/0-hello_world/src/HelloMessage.js
+++ code/schritte/0-hello_world/src/HelloMessage.js
@@ -2,16 +2,14 @@ import React from "react";
 
 export default function HelloMessage(props) {
   const [name, setName] = React.useState(props.initialGreeting || "");
-  const input = React.useRef();
 
   function reset() {
     setName("");
-    input.current.focus();
   }
 
   return (
     <div>
-      <input ref={input} onChange={event => setName(event.target.value)} value={name} />
+      <input onChange={event => setName(event.target.value)} value={name} />
 
       <p>{name}, World</p>
       <button onClick={reset}>Clear</button>
