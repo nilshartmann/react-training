bc8080ad02a0b9e4b4da3018c284645a85f700f2
Nils Hartmann <nils@nilshartmann.net>
2019-11-09 18:25:57 +0100

    blog example

---
+++ blog-example/workspace/src/App.js
@@ -0,0 +1,15 @@
+// Formular mit zwei Eingabefelden (title und body)
+// Formular muss im 'Container' div sein
+// ggf. header-Element
+//
+import React from "react";
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Hello React</h1>
+      </header>
+      <input />
+    </>
+  );
+}
