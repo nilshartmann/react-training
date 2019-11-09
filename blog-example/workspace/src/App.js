4517f12d5f6ba2a5365198a33db8c7d5a9e17c3c
Nils Hartmann <nils@nilshartmann.net>
2019-11-09 18:26:19 +0100

    fix last commit

--- blog-example/workspace/src/App.js
+++ blog-example/workspace/src/App.js
@@ -2,14 +2,3 @@
 // Formular muss im 'Container' div sein
 // ggf. header-Element
 //
-import React from "react";
-export default function App() {
-  return (
-    <>
-      <header>
-        <h1>Hello React</h1>
-      </header>
-      <input />
-    </>
-  );
-}
