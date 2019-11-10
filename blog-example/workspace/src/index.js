dcb80be4b94129526186ae7ba2249e40834290a4
Nils Hartmann <nils@nilshartmann.net>
2019-11-10 11:30:45 +0100

    handson

--- blog-example/workspace/src/index.js
+++ blog-example/workspace/src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// Entferne diese Komponente und ersetze sie durch die Blog-Anwendung
+// Wenn Du in App.js deine erste Komponente geschrieben hast, kannst Du
+// sie hier importieren und verwenden:
+//
+// import App from "./App";
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// dann entferne die folgenden Zeilen:
 function App() {
   return <h1>Hello World!</h1>;
 }
-
 ReactDOM.render(<App />, document.getElementById("root"));
