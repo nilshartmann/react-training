a7bc6190696d8f02c00127dfd03196135bd5ab32
Nils Hartmann <nils@nilshartmann.net>
2019-11-16 19:46:55 +0100

    Bugfixes aus dem Workshop

--- blog-example/backend/src/server.js
+++ blog-example/backend/src/server.js
@@ -27,6 +27,7 @@ let allPosts = [
 ];
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,PATCH,DELETE");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
