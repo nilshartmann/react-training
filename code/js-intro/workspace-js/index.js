7be2afb770cdc157d10c76ac2fcd37376886995d
Nils Hartmann <nils@nilshartmann.net>
2020-01-11 17:33:40 +0100

    übrung

--- code/js-intro/workspace-js/index.js
+++ code/js-intro/workspace-js/index.js
@@ -1,54 +1,56 @@
 console.log("Please edit index.js");
 
-// ######################## VOR OBJEKTEN
+// GOAL OF THIS EXERCISE:
+// ---------------------------------------------
+//  We have a generic verify-function that takes in a value
+//  and a list of "rules" (functions that checks a value if it meets a
+//  condition)
+//  The verify function uses the given "rule-functions" to determine
+//  if the given value is "correct" (each "rule-function" returned true)
+//
+//   So you could use this function for example to validate user input
+//   (the user input "must not be null" and "must be shorter than X chars")
 
-// HIER KENNEN WIR:
+// STEP 1: Implement two "rule" functions:
+//     - the first (isDefined) the checks if the value
+//        is "defined" (not null and not undefined),
+//     - the second (isNonEmpty) checks if the value is a string that has
+//       at least one character
 
-// FUNKTIONEN
-// ARRAYS inkl map und for
-// STRINGS
-// TRUTHY AND FALSY
+// Examples STEP 1:
+// console.log("RESULT isDefined, expected false => ", isDefined(null));
+// console.log("RESULT isDefined, expected false => ", isDefined(undefined));
+// console.log("RESULT isDefined, expected false => ", isDefined());
+// console.log("RESULT isDefined, expected true => ", isDefined(""));
+// console.log("RESULT isDefined, expected true => ", isDefined("Hello"));
 
-// step 1: write a function with one argument
-//         it should return true if the argument is a non-empty string
+// console.log("RESULT isNonEmpty, expected false => ", isNonEmpty(""));
+// console.log("RESULT isNonEmpty, expected true => ", isNonEmpty("Hello"));
 
-function isDefined(candidate) {
-  return candidate !== null && candidate !== undefined;
-}
+// ------------------------------------------------------------------------------------
+// STEP 2: Write the 'verify' function
+// The verify-function takes a value and a list (array) of functions that
+// should "verify" the given value
+//   - If at least one rule failes, the function should return false,
+//     otherwise true
+// Find examples below
 
-function isNonEmpty(candidate) {
-  return typeof candidate === "string" && candidate.length > 0;
-}
+// STEP 2a: If the caller of 'verify' does not specify the rules argument
+//           (like: verify("hello")) verify should return true
 
-// step X: write a verify function, that takes a value as first argument
-//   and an array of 'rule'-functions
-//   The function should verify the value against all rules.
-//   If at least one rule failes, the function should return false
-//   Otherwise return true
+// // EXAMPLES:
+// console.log("RESULT expected false => ", verify("", [isDefined, isNonEmpty]));
+// console.log("RESULT expected true  => ", verify("Hello", [isDefined, isNonEmpty]));
+// console.log("RESULT expected false  => ", verify(null, [isDefined, isNonEmpty]));
+// console.log("RESULT expected false  => ", verify(undefined, [isDefined, isNonEmpty]));
 
-// step X+1: if no rules (empty array or missing rules argument) have been given, the function should return true
+// // ...with Step 2a:
+// console.log("RESULT expected true => ", verify("", []));
+// console.log("RESULT expected true => ", verify(""));
+// console.log("RESULT expected true => ", verify("", null));
 
-function verify(candidate, rules) {
-  if (!rules) {
-    return true;
-  }
-  for (rule of rules) {
-    if (!rule(candidate)) return false;
-  }
-
-  return true;
-}
-
-console.log("RESULT expected false => ", verify("", [isDefined, isNonEmpty]));
-console.log("RESULT expected true  => ", verify("Hello", [isDefined, isNonEmpty]));
-console.log("RESULT expected false  => ", verify(null, [isDefined, isNonEmpty]));
-console.log("RESULT expected false  => ", verify(undefined, [isDefined, isNonEmpty]));
-
-// step X+1
-console.log("RESULT expected true => ", verify("", []));
-console.log("RESULT expected true => ", verify(""));
-
-// step Y: Write a 'verify all' function, that takes n values and verifies them against a list of rules
+// ------------------------------------------------------------------------------------
+// STEP 3: Write a 'verifyAll' function, that takes n values and verifies them against a list of rules
 //         (The rules should be the same for each value)
 //    It should return an array where each item in the array matches the verification result
 //      from the according value
@@ -56,15 +58,31 @@ console.log("RESULT expected true => ", verify(""));
 //    function verifyAll(values, rules) { ... }
 //    verifyAll(["Hello", null], [notEmpty]); // [true, false]
 //
-function verifyAll(values, rules) {
-  return values.map(value => verify(value, rules));
-}
+// STEP 3a:
+//      modify verifyAll: if the first parameter is NOT an array (but a single value)
+//      it should behave at if the first parameter would be a single-item array with that value
+//      Example: verifyAll(values, [isNonEmpty]) === verifyAll(values, isNonEmpty)
 
-console.log("RESULT expected false => ", verify("", [isDefined, isNonEmpty]));
-console.log("RESULT expected true  => ", verify("Hello", [isDefined, isNonEmpty]));
-console.log("RESULT expected false  => ", verify(null, [isDefined, isNonEmpty]));
-console.log("RESULT expected false  => ", verify(undefined, [isDefined, isNonEmpty]));
+// EXAMPLE RESULTS FOR STEP 3:
 
-// step Y+1
-// modify verifyAll: if the first parameter is NOT an array (but a single value)
-// it should behave at if the first parameter would be a single-item array with that value
+// console.log("RESULT expected [false, true] => ", verifyAll(["", "Hello"], [isDefined, isNonEmpty]));
+// console.log("RESULT expected [true] => ", verifyAll(["Hello"], [isDefined, isNonEmpty]));
+// console.log(
+//   "RESULT expected [true, false, false] => ",
+//   verifyAll(["Hello", null, ""], [isDefined, isNonEmpty])
+// );
+// console.log("RESULT expected [false] => ", verifyAll([undefined], [isDefined, isNonEmpty]));
+
+// // EXAMPLE RESULTS FOR STEP 3a:
+// //
+// console.log("RESULT expected [true]  => ", verifyAll("Hello", [isNonEmpty]));
+// console.log("RESULT expected [false]  => ", verifyAll("", [isNonEmpty]));
+
+// ------------------------------------------------------------------------------------
+// STEP 4: can you implement a "configurable" verifier with a different signature,
+//     like atLeastCharsLength(value, minLength) { ... }
+// how would you use this verify-Function either with verify oder verifyAll?
+
+// EXAMPLES FOR STEP 4:
+// console.log("RESULT expected true => ", verify("Hello", [atLeastFiveCharsLength]));
+// console.log("RESULT expected false  => ", verify("Ciao", [atLeastFiveCharsLength]));
---
+++ code/js-intro/workspace-js/solutions/01_arrays_and_functions.js
@@ -0,0 +1,120 @@
+console.log("Please edit index.js");
+
+// GOAL OF THIS EXERCISE:
+// ---------------------------------------------
+//  We have a generic verify-function that takes in a value
+//  and a list of "rules" (functions that checks a value if it meets a
+//  condition)
+//  The verify function uses the given "rule-functions" to determine
+//  if the given value is "correct" (each "rule-function" returned true)
+//
+//   So you could use this function for example to validate user input
+//   (the user input "must not be null" and "must be shorter than X chars")
+
+// STEP 1: Implement two "rule" functions:
+//     - the first (isDefined) the checks if the value
+//        is "defined" (not null and not undefined),
+//     - the second (isNonEmpty) checks if the value is a string that has
+//       at least one character
+function isDefined(candidate) {
+  return candidate !== null && candidate !== undefined;
+}
+
+function isNonEmpty(candidate) {
+  return typeof candidate === "string" && candidate.length > 0;
+}
+
+// Examples STEP 1:
+console.log("RESULT isDefined, expected false => ", isDefined(null));
+console.log("RESULT isDefined, expected false => ", isDefined(undefined));
+console.log("RESULT isDefined, expected false => ", isDefined());
+console.log("RESULT isDefined, expected true => ", isDefined(""));
+console.log("RESULT isDefined, expected true => ", isDefined("Hello"));
+
+console.log("RESULT isNonEmpty, expected false => ", isNonEmpty(""));
+console.log("RESULT isNonEmpty, expected true => ", isNonEmpty("Hello"));
+
+// ------------------------------------------------------------------------------------
+// STEP 2: Write the 'verify' function
+// The verify-function takes a value and a list (array) of functions that
+// should "verify" the given value
+//   - If at least one rule failes, the function should return false,
+//     otherwise true
+// Find examples below
+
+// STEP 2a: If the caller of 'verify' does not specify the rules argument
+//           (like: verify("hello")) verify should return true
+
+function verify(candidate, rules) {
+  if (!rules) {
+    return true;
+  }
+  for (rule of rules) {
+    if (!rule(candidate)) return false;
+  }
+
+  return true;
+}
+
+// EXAMPLES:
+console.log("RESULT expected false => ", verify("", [isDefined, isNonEmpty]));
+console.log("RESULT expected true  => ", verify("Hello", [isDefined, isNonEmpty]));
+console.log("RESULT expected false  => ", verify(null, [isDefined, isNonEmpty]));
+console.log("RESULT expected false  => ", verify(undefined, [isDefined, isNonEmpty]));
+
+// ...with Step 2a:
+console.log("RESULT expected true => ", verify("", []));
+console.log("RESULT expected true => ", verify(""));
+console.log("RESULT expected true => ", verify("", null));
+
+// ------------------------------------------------------------------------------------
+// STEP 3: Write a 'verifyAll' function, that takes n values and verifies them against a list of rules
+//         (The rules should be the same for each value)
+//    It should return an array where each item in the array matches the verification result
+//      from the according value
+//    example:
+//    function verifyAll(values, rules) { ... }
+//    verifyAll(["Hello", null], [notEmpty]); // [true, false]
+//
+// STEP 3a:
+//      modify verifyAll: if the first parameter is NOT an array (but a single value)
+//      it should behave at if the first parameter would be a single-item array with that value
+//      Example: verifyAll(values, [isNonEmpty]) === verifyAll(values, isNonEmpty)
+function verifyAll(values, rules) {
+  // 3a
+  if (!Array.isArray(values)) {
+    values = [values];
+  }
+
+  // 3
+  return values.map(value => verify(value, rules));
+}
+
+// EXAMPLE RESULTS FOR STEP 3:
+
+console.log("RESULT expected [false, true] => ", verifyAll(["", "Hello"], [isDefined, isNonEmpty]));
+console.log("RESULT expected [true] => ", verifyAll(["Hello"], [isDefined, isNonEmpty]));
+console.log(
+  "RESULT expected [true, false, false] => ",
+  verifyAll(["Hello", null, ""], [isDefined, isNonEmpty])
+);
+console.log("RESULT expected [false] => ", verifyAll([undefined], [isDefined, isNonEmpty]));
+
+// EXAMPLE RESULTS FOR STEP 3a:
+//
+console.log("RESULT expected [true]  => ", verifyAll("Hello", [isNonEmpty]));
+console.log("RESULT expected [false]  => ", verifyAll("", [isNonEmpty]));
+
+// ------------------------------------------------------------------------------------
+// STEP 4: can you implement a "configurable" verifier with a different signature,
+//     like atLeastCharsLength(value, minLength) { ... }
+// how would you use this verify-Function either with verify oder verifyAll?
+function atLeastCharsLength(value, minLength) {
+  return value === "string" && value.length >= minLength;
+}
+
+const atLeastFiveCharsLength = value => atLeastCharsLength(value, 5);
+
+// EXAMPLES FOR STEP 4:
+console.log("RESULT expected true => ", verify("Hello", [atLeastFiveCharsLength]));
+console.log("RESULT expected false  => ", verify("Ciao", [atLeastFiveCharsLength]));
