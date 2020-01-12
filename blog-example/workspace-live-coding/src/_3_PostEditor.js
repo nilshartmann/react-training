dfc0da8800927c7e3f5bb1239a119ec3e7120d3d
Nils Hartmann <nils@nilshartmann.net>
2020-01-12 21:17:37 +0100

    übungen

---
+++ blog-example/workspace-live-coding/src/_3_PostEditor.js
@@ -0,0 +1,65 @@
+import React from "react";
+import moment from "moment";
+
+// ======================================================================================================
+//  POST EDITOR
+// ======================================================================================================
+
+export default function PostEditor() {
+  const [title, setTitle] = React.useState("");
+  const [body, setBody] = React.useState("");
+
+  const titleRef = React.useRef();
+  const cancelDisabled = !title && !body;
+
+  function clear() {
+    setTitle("");
+    setBody("");
+    titleRef.current.focus();
+  }
+
+  return (
+    <div className="Container">
+      <h1>Create Post</h1>
+
+      <label>
+        Title
+        <input ref={titleRef} value={title} onChange={e => setTitle(e.currentTarget.value)} />
+      </label>
+
+      <label>
+        Body
+        <textarea value={body} onChange={e => setBody(e.currentTarget.value)} />
+      </label>
+
+      <button disabled={cancelDisabled} onClick={clear}>
+        Clear
+      </button>
+    </div>
+  );
+}
+
+// ======================================================================================================
+//  POST LIST
+// ======================================================================================================
+
+export function PostList(props) {
+  const posts = props.posts;
+
+  return (
+    <>
+      <button onClick={props.onAddPost}>Add Post</button>
+      {posts.map(p => (
+        <article key={p.id} className="Container">
+          <p className="Date">{formattedDate(p.date)}</p>
+          <h1>{p.title}</h1>
+          <p>{p.body}</p>
+        </article>
+      ))}
+    </>
+  );
+}
+
+function formattedDate(date) {
+  return moment(date).format("DD.MM.YYYY");
+}
---
+++ blog-example/workspace-live-coding/src/_3_index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import PostEditor, { PostList } from "./_3_PostEditor";
+
+import mockPosts from "./mock";
+
+ReactDOM.render(<PostEditor />, document.getElementById("root"));
+
+if (false) {
+  ReactDOM.render(<PostList posts={mockPosts} />, document.getElementById("root"));
+}
