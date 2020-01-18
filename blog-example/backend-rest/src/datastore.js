bf599e09c32efed91672a82fbfd5ae096b1103b8
Nils Hartmann <nils@nilshartmann.net>
2020-01-18 21:33:35 +0100

    enhance rest backend with authorization and datastore

---
+++ blog-example/backend-rest/src/datastore.js
@@ -0,0 +1,111 @@
+const { readPosts, readUsers } = require("./example-post-reader");
+/** User
+ *  @typedef {Object} User
+ *  @property {string} id - The unique id
+ *  @property {string} login - The login
+ *  @property {string} name - The fullname of this User
+ */
+
+/** BlogPost
+ *  @typedef {Object} BlogPost
+ *  @property {string} id - The unique id
+ *  @property {string} title - The title
+ *  @property {string} date - Publication date
+ *  @property {string} body - Body
+ *  @property {string} userId - Reference to user
+ *  @property {number} likes - Likes!
+ */
+
+/** @type {User[]} */
+const users = readUsers();
+/** @type {Map<string,BlogPost>} */
+const posts = readPosts();
+
+function getAllPosts() {
+  const allPosts = [...posts.values()];
+  allPosts.sort((p1, p2) => new Date(p2.date) - new Date(p1.date));
+
+  return allPosts;
+}
+
+function getPost(postId) {
+  return posts.get(postId);
+}
+
+function getAllUsers() {
+  return users;
+}
+
+function getUser(userId) {
+  return users.find(u => u.id === userId);
+}
+
+function getUserByLogin(login) {
+  return users.find(u => u.login === login);
+}
+
+function likePost(postId) {
+  const post = posts.get(postId);
+
+  if (!post) {
+    throw new Error(`Cannot find BlogPost '${postId}'`);
+  }
+
+  const updatedPost = {
+    ...post,
+    likes: post.likes + 1
+  };
+
+  posts.set(postId, updatedPost);
+
+  return updatedPost;
+}
+
+function updatePost(postData) {
+  const post = posts.get(postData.id);
+
+  if (!post) {
+    return null;
+  }
+
+  const updatedPost = {
+    ...post,
+    title: postData.title,
+    body: postData.body
+  };
+
+  posts.set(post.id, updatedPost);
+
+  return updatedPost;
+}
+
+function deletePost(postId) {
+  return posts.delete(postId);
+}
+
+function insertPost(userId, { title, body }) {
+  const newPost = {
+    userId,
+    title,
+    body,
+    likes: 0,
+    date: new Date().toISOString(),
+    id: `P${posts.size + 1}`
+  };
+
+  posts.set(newPost.id, newPost);
+
+  return newPost;
+}
+
+module.exports = {
+  getAllPosts,
+  getAllUsers,
+  getUser,
+  getUserByLogin,
+  insertPost,
+  updatePost,
+  deletePost,
+  likePost,
+  getPost
+};
--- blog-example/backend-rest/src/server.js
+++ blog-example/backend-rest/src/server.js
@@ -1,81 +1,26 @@
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const datastore = require("./datastore");
+
 const app = express();
 
+const authEnabled = process.env.USE_AUTH === "true";
+/** IN REAL LIVE YOU WILL NEVER STORE JWT_SECRET IN YOUR CODE! */
+const JWT_SECRET = "hurzelpurzel";
+
+function tokenFor(userId) {
+  const token = jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: "10 years"
+  });
+
+  return token;
+}
+
 app.use(bodyParser.json());
-let allPosts = [
-  {
-    id: "8",
-    date: "2019-10-23T18:25:43.511Z",
-    title: "Consider async/await over promises",
-    body: `I also believe it's important for every member to be involved and invested in our company and this is one way to do so. Curate.
-        Guerrilla marketing we don't want to boil the ocean we need to leverage our synergies touch base
-        The sprint is over please use "solutionise" instead of solution ideas! :)
-        Push back digitalize yet enough to wash your face, or low-hanging fruit horsehead offer, for Bob called an all-hands this afternoon that ipo will be a game-changer.
-        `
-  },
-  {
-    id: "7",
-    date: "2019-11-22T14:12:21.511Z",
-    title: "Learning React...",
-    body: `Self law truth moral will gains. Marvelous self burying battle virtues eternal-return.
-        Chaos of madness ultimate moral moral play victorious faith ubermensch pious will.
-        Zarathustra will burying christianity enlightenment decrepit christian ocean gains. Good ocean strong  grandeur free superiority zarathustra selfish inexpedient reason.
-        Decrepit ultimate chaos.
-        `
-  },
-  {
-    id: "6",
-    date: "2019-06-12T14:10:22.666Z",
-    title: "Do's and don'ts when using React Router",
-    body: `Burying marvelous good against spirit passion reason. Value hope superiority derive hatred justice eternal-return zarathustra revaluation fearful christianity faith abstract hope.
-Depths transvaluation joy fearful sea. Prejudice god merciful hope intentions sexuality oneself revaluation contradict christianity war philosophy passion. Convictions of of free insofar battle intentions ascetic decieve derive good. Madness sexuality deceptions faithful christianity victorious.
-Will victorious self against madness holiest right against ultimate reason revaluation god decieve. Inexpedient law eternal-return convictions sexuality love play marvelous enlightenment joy. Ultimate reason evil christian pinnacle endless contradict. Snare love horror overcome ascetic revaluation enlightenment derive sea overcome. Derive of horror disgust truth revaluation deceptions play disgust god morality abstract passion pious. Against dead reason faithful ocean hope abstract of pinnacle.
-Chaos law hope endless ocean virtues ascetic. Transvaluation deceptions intentions justice hatred salvation good war society justice. Ultimate pious selfish virtues strong love contradict god grandeur enlightenment.`
-  },
-  {
-    id: "5",
-    date: "2019-07-07T12:22:22.666Z",
-    title: "Gotchas of React Hooks API",
-    body: `Eat all the power cords get scared by sudden appearance of cucumber for howl uncontrollably for no reason yet i cry and cry and cry unless you pet me, and then maybe i cry just for fun pooping rainbow while flying in a toasted bread costume in space. 
-    Meow for food, then when human fills food dish, take a few bites of food and continue meowing behind the couch, so sit in a box for hours but find a way to fit in tiny box you have cat to be kitten me right meow for make meme, make cute face.
-`
-  },
-  {
-    id: "4",
-    date: "2019-07-12T13:22:22.666Z",
-    title: "GraphQL primer",
-    body: `Swine bresaola porchetta drumstick. 
-    Doner flank pork, burgdoggen jerky venison bresaola corned beef landjaeger t-bone hamburger pastrami tenderloin. 
-    Short ribs chuck andouille cow t-bone doner ribeye tail bacon shank meatloaf pork tenderloin capicola frankfurter. 
-    Andouille hamburger pastrami, cow bacon kielbasa chicken pancetta sirloin landjaeger pork belly.`
-  },
-  {
-    id: "2",
-    date: "2019-08-23T18:25:43.511Z",
-    title: "Writing React applications using TypeScript",
-    body: `I also believe it's important for every member to be involved and invested in our company and this is one way to do so. Curate.
-        Guerrilla marketing we don't want to boil the ocean we need to leverage our synergies touch base
-        The sprint is over please use "solutionise" instead of solution ideas! :)
-        Push back digitalize yet enough to wash your face, or low-hanging fruit horsehead offer, for Bob called an all-hands this afternoon that ipo will be a game-changer.
-        `
-  },
-  {
-    id: "1",
-    date: "2019-09-22T14:12:21.511Z",
-    title: "Modern Frontend Development",
-    body: `Self law truth moral will gains. Marvelous self burying battle virtues eternal-return.
-        Chaos of madness ultimate moral moral play victorious faith ubermensch pious will.
-        Zarathustra will burying christianity enlightenment decrepit christian ocean gains. Good ocean strong  grandeur free superiority zarathustra selfish inexpedient reason.
-        Decrepit ultimate chaos.
-        `
-  }
-];
 
-let counter = allPosts.length + 1000;
-
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,PATCH,DELETE");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -94,69 +39,109 @@ app.use((req, _res, next) => {
   }
 });
 
-function sortByDate(posts) {
-  return posts.slice().sort((p1, p2) => new Date(p2.date).getTime() > new Date(p1.date).getTime());
+if (authEnabled) {
+  app.use((req, _res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+      const payload = jwt.verify(token, JWT_SECRET);
+      const user = datastore.getUser(payload.userId);
+      if (!user) {
+        return res.status(401).json({ error: `User with id '${payload.userId}' not found` });
+      }
+      req.user = user;
+      req.userId = user.id;
+    }
+    next();
+  });
+
+  app.post("/login", (req, res) => {
+    const login = req.body;
+
+    if (!login) {
+      return res.status(400).json({ error: "login (payload) must be defined" });
+    }
+
+    if (!login.login) {
+      return res.status(400).json({ error: "login.login must be defined" });
+    }
+
+    if (!login.password) {
+      return res.status(400).json({ error: "login.password must be defined" });
+    }
+
+    const user = datastore.getUserByLogin(login.login);
+
+    if (!user) {
+      return res.status(403).json({ error: "Invalid user" });
+    }
+
+    return res.status(200).json({
+      token: tokenFor(user.id),
+      user
+    });
+  });
 }
 
 app.get("/posts", (req, res) =>
-  res.json(
-    sortByDate(
-      req.query.short !== undefined
-        ? allPosts.map(p => ({ id: p.id, date: p.date, title: p.title }))
-        : allPosts
+  res
+    .status(200)
+    .json(
+      req.query.short === undefined
+        ? datastore.getAllPosts()
+        : datastore.getAllPosts().map(p => ({ id: p.id, date: p.date, title: p.title }))
     )
-  )
 );
 
 // Return Post with specified id (or 404)
 app.get("/posts/:id", (req, res) => {
-  const post = allPosts.find(p => p.id === req.params.id);
+  const post = datastore.getPost(req.params.id);
 
   if (!post) {
     return res.status(404).json({ error: `Post '${req.params.id}' not found` });
   }
 
-  return res.json(post);
+  return res.status(200).json(post);
 });
 
 app.post("/posts", (req, res) => {
+  if (authEnabled && !req.user) {
+    return res.status(401).json({ error: "use must be logged in to execute this action" });
+  }
+
   const post = req.body;
   if (!post) {
     return res.status(400).json({ error: "Post must be defined" });
   }
 
   if (!post.title) {
-    return res.status(400).json({ error: "post.title must be defined" });
+    return res.status(400).json({ error: "post.title must be defined and not empty" });
   }
 
   if (!post.body) {
-    return res.status(400).json({ error: "post.body must be defined" });
+    return res.status(400).json({ error: "post.body must be defined and not empty" });
   }
 
-  const newPost = {
-    ...post,
-    id: String(counter++),
-    date: new Date().toJSON()
-  };
+  const newPost = datastore.insertPost(req.userId, post);
 
   if (post.title.startsWith("slow")) {
     // slow down greetings with a
     // to show async problems in App
 
     setTimeout(() => {
-      allPosts = [newPost, ...allPosts];
       res.status(201).json(newPost);
     }, 10000);
 
     return;
   }
 
-  allPosts = [newPost, ...allPosts];
-
   res.status(201).json(newPost);
 });
 
 app.put("/posts", (req, res) => {
+  if (authEnabled && !req.user) {
+    return res.status(401).json({ error: "use must be logged in to execute this action" });
+  }
+
   const post = req.body;
   if (!post) {
     return res.status(400).json({ error: "Post must be defined" });
@@ -174,34 +159,43 @@ app.put("/posts", (req, res) => {
     return res.status(400).json({ error: "post.body must be defined" });
   }
 
-  let updatedPost = null;
+  const updatedPost = datastore.updatePost(post);
 
-  allPosts = allPosts.map(p => {
-    if (p.id === post.id) {
-      updatedPost = { ...p, title: post.title, body: post.body };
-      return updatedPost;
-    }
-    return p;
-  });
+  console.log("UPDATE", updatedPost);
 
   if (!updatedPost) {
     return res.status(404).json({ error: `Post with id ${post.id} not found` });
   }
-
-  res.status(201).json(updatedPost);
+  res.status(200).json(updatedPost);
 });
 
 app.delete("/posts/:id", (req, res) => {
+  if (authEnabled && !req.user) {
+    return res.status(401).json({ error: "use must be logged in to execute this action" });
+  }
+
   const postId = req.params.id;
-  allPosts = allPosts.filter(p => p.id !== postId);
+  if (!postId) {
+    return res.status(400).json({ error: "postId must be defined in the URL" });
+  }
+
+  if (!datastore.deletePost(postId)) {
+    return res.status(404).json({ error: `Post with id ${postId} not found` });
+  }
   res.sendStatus(200);
 });
 
 const port = process.env.SERVER_PORT || 7000;
 
 app.listen(port, () => {
+  const authHints = authEnabled
+    ? `
+    🚓    Auth Mode is enabled
+    🔑    Auth-Header: 'Authorization: ${tokenFor("U2")}'`
+    : "";
+
   console.log(`
-    📞    Blogging API Server listening on port ${port}
+    📞    Blogging API Server listening on port ${port}${authHints}
     👉    Try http://localhost:${port}/posts
 `);
 });
