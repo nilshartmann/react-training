1549d4740a9a832dcee6aa1360e6c53f9ef6e0ed
Nils Hartmann <nils@nilshartmann.net>
2020-06-16 21:56:57 +0200

    workspace-typescript

---
+++ advanced/workspace-typescript/src/PostEditor.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostEditor() {
+  return <div className="Container"></div>;
+}
---
+++ advanced/workspace-typescript/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import PostEditor from "./PostEditor";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PostEditor />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
