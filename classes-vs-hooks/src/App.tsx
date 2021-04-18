4ff480892facb01f9921d230a0db4e04924b2050
Nils Hartmann <nils@nilshartmann.net>
2021-04-18 09:15:36 +0200

    re-org examples

--- classes-vs-hooks/src/App.tsx
+++ classes-vs-hooks/src/App.tsx
@@ -1,40 +1,36 @@
 import * as React from "react";
-import ClassApp from "./classes/ClassApp";
-import HooksApp from "./hooks/HooksApp";
-import HooksAppAsync from "./hooks_rendering/HooksAppRendering";
-import HooksTimer from "./hooks_timer/HooksTimer";
-import HooksTimerWithCancel from "./hooks_timer_cancel/HooksTimerWithCancel";
+import ClassApp from "./01_classes/ClassApp";
+import HooksApp from "./02_hooks/HooksApp";
+import HooksAppRendering from "./03_hooks_rendering/HooksAppRendering";
+import HooksTimer from "./04_hooks_timer/HooksTimer";
+import HooksTimerWithCancel from "./05_hooks_timer_cancel/HooksTimerWithCancel";
 
 export default function App() {
   const [example, setExample] = React.useState(-1);
 
+  const examples = [
+    ["Class Example", <ClassApp />],
+    ["Hook Example", <HooksApp />],
+    ["Hook Example Rendering", <HooksAppRendering />],
+    ["Hook Timer", <HooksTimer />],
+    ["HooksTimerWithCancel", <HooksTimerWithCancel />]
+  ];
+
   return (
     <div>
       <div className="Container">
-        <button className={example === 0 ? "Active" : undefined} onClick={() => setExample(0)}>
-          Class Example
-        </button>
-        <button className={example === 1 ? "Active" : undefined} onClick={() => setExample(1)}>
-          Hook Example
-        </button>
+        {examples.map((ex, ix) => (
+          <button
+            key={ix}
+            className={example === ix ? "Active" : undefined}
+            onClick={() => setExample(ix)}
+          >
+            {ex[0]}
+          </button>
+        ))}
+      </div>
 
-        <button className={example === 2 ? "Active" : undefined} onClick={() => setExample(2)}>
-          Hook Example Rendering
-        </button>
-        <button className={example === 3 ? "Active" : undefined} onClick={() => setExample(3)}>
-          Hook Timer
-        </button>
-        <button className={example === 4 ? "Active" : undefined} onClick={() => setExample(4)}>
-          Hook Timer with Cancel
-        </button>
-      </div>
-      <div className="Container">
-        {example === 0 && <ClassApp />}
-        {example === 1 && <HooksApp />}
-        {example === 2 && <HooksAppAsync />}
-        {example === 3 && <HooksTimer />}
-        {example === 4 && <HooksTimerWithCancel />}
-      </div>
+      {example !== -1 && <div className="Container">{examples[example][1]}</div>}
     </div>
   );
 }
