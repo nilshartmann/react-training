c42620eb8d1fad2bb5f2fc31767c3cb64924a331
Nils Hartmann <nils@nilshartmann.net>
2021-06-11 08:52:07 +0200

    fix livecoding workspace

--- blog-example/workspace-live-coding/src/index.js
+++ blog-example/workspace-live-coding/src/index.js
@@ -1,11 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 import "./index.css";
-import App from "./PostEditor";
-
-import mockPosts from "./mock";
-
-// ReactDOM.render(<PostEditor />, document.getElementById("root"));
-
-ReactDOM.render(<App />, document.getElementById("root"));
