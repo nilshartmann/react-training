5861386b39b6540e650f78625440fb49c9f1cec1
Nils Hartmann <nils@nilshartmann.net>
2022-02-09 17:16:35 +0100

    Revert "Upgrade React, TypeScript react-scripts"
    This reverts commit 8683fcd74489f1960f1ad44a826664d2a28a7549.
    
    # Conflicts:
    #	blog-example/steps/9-graphql/package.json
    #	blog-example/steps/9b-graphql-with-typescript/package-lock.json
    #	blog-example/workspace-graphql/package-lock.json
    #	blog-example/workspace-graphql/package.json

--- blog-example/build-and-install.zsh
+++ blog-example/build-and-install.zsh
@@ -6,17 +6,5 @@ echo "INSTALL IN $HERE"
 
 cd $HERE
 rm -rf node_modules
-
-npm install --save --save-exact react-scripts@5.0.0 @testing-library/jest-dom@5.16.1 @testing-library/react@12.1.2 @testing-library/user-event@13.5.0
-npm install --save --save-exact react@17 react-dom@17 react-test-renderer@17
-
-if [[ -f "ts.config" ]]; then
-    echo "UPGRADING TYPESCRIPT"
-    npm install --save --save-exact  typescript@4.5.4 @types/jest@27.4.0 @types/node@16.11.19 @types/react@17.0.38 @types/react-dom@17.0.11
-fi
-
 npm install
 npm run build
-
-
-
